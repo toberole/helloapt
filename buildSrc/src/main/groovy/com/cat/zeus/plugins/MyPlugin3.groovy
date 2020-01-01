386f48b81bda27a1f8de337bb7a40b0db63fce2c
zhouwei441300 <zhouwei441300@163.com>
2020-01-01 18:18:48 +0800

    note

--- buildSrc/src/main/groovy/com/cat/zeus/plugins/MyPlugin3.groovy
+++ buildSrc/src/main/groovy/com/cat/zeus/plugins/MyPlugin3.groovy
@@ -13,18 +13,19 @@ class MyPlugin3 extends BasePlugin {
     @Override
     void transform(TransformInvocation transformInvocation) throws TransformException, InterruptedException, IOException {
         super.transform(transformInvocation)
-
-        System.out.println("----------开始Transform-----------")
-        // Transform 的 inputs 分为两种类型，一直是目录，一种是 jar 包。需要分开遍历
-
+        // Transform 的 inputs 分为两种类型
+        // 一种是目录
+        // 一种是 jar包[依赖的jar包或者是自建的moudle]
         transformInvocation.inputs.each { TransformInput input ->
             // 1) 对类型为"目录"的 input 进行遍历
             input.directoryInputs.each { DirectoryInput dirInput ->
-                // demo1. 在MainActivity的onCreate()方法之前注入代码
+                // 在MainActivity的onCreate()方法之前注入代码
                 MyInject.injectOnCreate(dirInput.file.absolutePath, project)
                 // 获取 output 目录
-                def dest = transformInvocation.outputProvider.getContentLocation(dirInput.name, dirInput.contentTypes,
-                        dirInput.scopes, Format.DIRECTORY)
+                def dest = transformInvocation.outputProvider.getContentLocation(
+                        dirInput.name, dirInput.contentTypes,
+                        dirInput.scopes, Format.DIRECTORY
+                )
                 // 将 input 的目录复制到 output 指定目录
                 FileUtils.copyDirectory(dirInput.file, dest)
             }
@@ -32,7 +33,6 @@ class MyPlugin3 extends BasePlugin {
             // 2) 对类型为 jar 文件的 input 进行遍历
             input.jarInputs.each { JarInput jarInput ->
                 // jar 文件一般是第三方依赖库jar包
-
                 // 重命名输出文件（同目录 copyFile 会冲突）
                 def jarName = jarInput.name
                 def md5Name = DigestUtils.md5Hex(jarInput.file.getAbsolutePath())
@@ -41,19 +41,21 @@ class MyPlugin3 extends BasePlugin {
                     jarName = jarName.substring(0, jarName.length() - 4)
                 }
                 // 生成输出路径
-                def dest = transformInvocation.outputProvider.getContentLocation(jarName + md5Name, jarInput.contentTypes,
-                        jarInput.scopes, Format.JAR)
+                def dest = transformInvocation.outputProvider.getContentLocation(
+                        jarName + md5Name, jarInput.contentTypes,
+                        jarInput.scopes, Format.JAR
+                )
                 // 将输入内容复制到输出
                 FileUtils.copyFile(jarInput.file, dest)
             }
         }
-
-        System.out.println("----------结束Transform-----------")
     }
 
     /**
      * 核心方法，具体如何处理输入和输出
      * @param inputs 为传过来的输入流，两种格式，一种jar包格式，一种目录格式
-     * @param outputProvider 获取到输出目录，最后将修改的文件复制到输出目录，这一步必须执行，不让编译会报错
+     * @param outputProvider 获取到输出目录，最后将修改的文件复制到输出目录，
+     * 作为下一个Transdorm的输入，以供下一个Transfrom使用，
+     * 这一步必须执行，不让编译会报错
      */
 }
